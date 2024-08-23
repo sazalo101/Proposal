6c3db1e075073f14e28b8ddacacfa31fae80085b
sazalo101 <samuelaondo10@gmail.com>
2024-08-23 17:54:11 +0300

    PEFERCTO verified SAZA

--- test.sol
+++ test.sol
@@ -1,24 +1,84 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+
 contract ProposalContract {
-   uint256 private counter;
+    // ****************** Data ***********************
+
+    //Owner
+    address owner;
+
+    uint256 private counter;
 
     struct Proposal {
-        string title;
-        string description; 
-        uint256 approve; 
-        uint256 reject; 
-        uint256 pass; 
-        uint256 total_vote_to_end;
-        bool current_state; 
-        bool is_active; 
+        string description; // Description of the proposal
+        uint256 approve; // Number of approve votes
+        uint256 reject; // Number of reject votes
+        uint256 pass; // Number of pass votes
+        uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
+        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
+        bool is_active; // This shows if others can vote to our contract
     }
 
-    mapping(uint256 => Proposal) proposal_history; 
+    mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
 
-    function create(string calldata _title,string calldata _description, uint256 _total_vote_to_end) external {
+    address[] private voted_addresses; 
+
+    //constructor
+    constructor() {
+        owner = msg.sender;
+        voted_addresses.push(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier active() {
+        require(proposal_history[counter].is_active == true);
+        _;
+    }
+
+    modifier newVoter(address _address) {
+        require(!isVoted(_address), "Address has already voted");
+        _;
+    }
+
+
+//     // ****************** Execute Functions ***********************
+
+
+    function setOwner(address new_owner) external onlyOwner {
+        owner = new_owner;
+    }
+
+    function create(string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
         counter += 1;
-        proposal_history[counter] = Proposal(_title,_description, 0, 0, 0, _total_vote_to_end, false, true);
+        proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
+    }
+    
+
+    function vote(uint8 choice) external active newVoter(msg.sender){
+        Proposal storage proposal = proposal_history[counter];
+        uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
+
+        voted_addresses.push(msg.sender);
+
+        if (choice == 1) {
+            proposal.approve += 1;
+            proposal.current_state = calculateCurrentState();
+        } else if (choice == 2) {
+            proposal.reject += 1;
+            proposal.current_state = calculateCurrentState();
+        } else if (choice == 0) {
+            proposal.pass += 1;
+            proposal.current_state = calculateCurrentState();
+        }
+
+        if ((proposal.total_vote_to_end - total_vote == 1) && (choice == 1 || choice == 2 || choice == 0)) {
+            proposal.is_active = false;
+            voted_addresses = [owner];
+        }
     }
 }
